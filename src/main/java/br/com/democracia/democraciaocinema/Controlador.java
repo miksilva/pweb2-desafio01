dfb1aab276e0847f210bb160249a60caf71393d2
leofernandesmo <leofernandesmo@gmail.com>
2019-11-26 19:17:47 -0300

    Primeiro commit do projeto de Pweb2

---
+++ src/main/java/br/com/democracia/democraciaocinema/Controlador.java
@@ -0,0 +1,33 @@
+package br.com.democracia.democraciaocinema;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+public class Controlador{
+
+    @RequestMapping("/formulario")
+    public ModelAndView formulario() {
+        return new ModelAndView("formulario.html");
+    }
+    
+    @RequestMapping("/resultado")
+    public ModelAndView resultado(String nome, String senha){
+        String novaSenha = senha.replaceAll(" ", "");
+        
+        ModelAndView mv = new ModelAndView("resultado.html");
+        String novoNome = nome.replaceAll(" ", "");
+        mv.addObject("nome", "Seu nome tem " + novoNome.length() + " letras");
+        if (novoNome.equals("") || novaSenha.equals("")) {
+            ModelAndView mj = new ModelAndView("formulario.html");
+            mj.addObject("erro1","Os dois campos são obrigatorios");
+            return mj;
+        }
+       
+        return mv;
+        
+    }
+
+
+}
---
+++ src/test/java/br/com/democracia/democraciaocinema/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.democracia.democraciaocinema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
